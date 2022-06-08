2fb7424cacd5708336481af1df330f7dc82c96b2
faiyaj <faiyajz007@gmail.com>
2022-06-08 15:10:04 +0600

    Movie Search App- movie from static json data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import {useState, useEffect} from 'react';
+import {useEffect, useState} from "react";
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
+const API_URL = 'http://www.omdbapi.com/?apikey=115d5f09';
+
+    const movie = {
+        "Title":"Harry Potter and the Deathly Hallows: Part 2",
+        "Year":"2011",
+        "imdbID":"tt1201607",
+        "Type":"movie",
+        "Poster":"https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+    }
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    // alert('Reloaded')
-    // setCounter(100);
-    alert("You've changed the counter to " + counter)
-  },[counter]);
 
-  return (
-    <div className="App">
-        <button onClick={() => setCounter( (prevCount) => prevCount - 1 )}> - </button>
-        {/* <button onClick={()=>alert('Minus Button Clikcked')}> - </button> */}
+    const searchMovies = async(title) => {
         
-        <h1>{counter}</h1>
+        const response = await fetch(`${API_URL}&s=${title}`);
         
-        {/* <button onClick={()=>alert('Plus Button Clikcked')}> + </button> */}
-        <button onClick={() =>setCounter( (prevCount) => prevCount + 1 )}> + </button>
+        const data = await response.json();
+        
+        console.log(data.Search);
+
+    }
 
 
-    </div>
-  );
+    useEffect( () => {
+        searchMovies('Harry Potter');
+    },[]);
+
+    return(
+      <div className="app">
+          <h1>Movie Search App</h1>
+          
+          <div className="search">
+              <input 
+                placeholder="Search for movies"
+                value="Harry Potter"
+                onChange={ () => {} }  
+              />
+              <img 
+                src={SearchIcon}
+                alt= "Search"
+                onClick={() => {}}
+              />
+          </div>
+          
+          <div className="container">
+            <MovieCard movie={movie}/>
+          </div>
+      </div>  
+    );
 }
 
 export default App;
