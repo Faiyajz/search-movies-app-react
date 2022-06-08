5023780705893bd16225acdff06a125aae426cb8
faiyaj <faiyajz007@gmail.com>
2022-06-08 09:35:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https:faiyajz.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Md. Faiyaj Zaman
         </a>
       </header>
     </div>
